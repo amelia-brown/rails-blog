83de1e26e37ffa914be754e7a95d36f8737eea75
Amelia Brown <amelia.brown389@gmail.com>
2016-04-12 23:49:22 -0400

    (fix): remove references from pages table

--- db/migrate/20160409233640_create_pages.rb
+++ db/migrate/20160409233640_create_pages.rb
@@ -1,7 +1,6 @@
 class CreatePages < ActiveRecord::Migration
   def change
     create_table :pages do |t|
-      t.references :content_block, index: true, foreign_key: true
       t.string :title
 
       t.timestamps null: false
