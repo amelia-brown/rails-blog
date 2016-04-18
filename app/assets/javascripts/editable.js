93775dd7e918df1646d7b1566ccdbede924c0a25
Amelia Brown <amelia.brown389@gmail.com>
2016-04-18 12:17:39 -0400

    (feature): edit toggling

--- app/assets/javascripts/editable.js
+++ app/assets/javascripts/editable.js
@@ -27,28 +27,52 @@ $(document).ready(function() {
   };
 
   $('.edit').click(function() {
-    if (window.location.pathname === "/work") {
-      $.ajax({
-        url: "/work",
-        responseTime:200,
-        dataType: 'json',
-        type: 'put',
-        success: function(settings) {
-          console.log(settings);
+    var iseditable = $(this).data("editable");
+    var content_block = $(this).data("pk");
+    var path = window.location.pathname;
+    var content = $(this).data("content");
+    $.ajax({
+      url: path,
+      responseTime:200,
+      dataType: 'json',
+      type: 'put',
+      data: JSON.stringify([
+        { pk: content_block,
+          content: content,
+          editable: iseditable
         }
-      });
-    } else if (window.location.pathname === "/about") {
-      $.ajax({
-        url: "/about",
-        responseTime:200,
-        dataType: 'json',
-        type: 'put',
-        success: function(settings) {
-          console.log(settings);
-        }
-      });
-    } else {
-      return;
-    }
+      ]),
+      headers: {
+        "pk": content_block
+      },
+      success: function(settings) {
+        console.log(settings);
+      }
+    });
   });
+//    if (window.location.pathname === "/work") {
+//      $.ajax({
+//        url: "/work",
+//        responseTime:200,
+//        dataType: 'json',
+//        type: 'put',
+//        success: function(settings) {
+//          console.log(settings);
+//        }
+//      });
+//    } else if (window.location.pathname === "/about") {
+//      $.ajax({
+//        url: "/about",
+//        responseTime:200,
+//        dataType: 'json',
+//        type: 'put',
+//        success: function(settings) {
+//          console.log(settings);
+//        }
+//      });
+//    } else {
+//      return;
+//    }
+//  });
+
 });
