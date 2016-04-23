afafe526ca71151f006e2108a3552cbd464008d7
Amelia Brown <amelia.brown389@gmail.com>
2016-04-23 15:24:49 -0400

    (feature): style feed/posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def show
   end
   def home
-    WillPaginate.per_page = 10
+    WillPaginate.per_page = 5
     @posts = User.first.posts.all.paginate(page: params[:page])
     @post = User.first.posts.build if logged_in?
   end
