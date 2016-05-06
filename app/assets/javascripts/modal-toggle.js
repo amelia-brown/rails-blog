373bfac07257740fd6cd433e2fddb6e50ceb85a6
Amelia Brown <amelia.brown389@gmail.com>
2016-05-06 14:56:48 -0400

    (feature): photo upload preview

--- app/assets/javascripts/modal-toggle.js
+++ app/assets/javascripts/modal-toggle.js
@@ -2,4 +2,31 @@ $(document).ready(function() {
   $(".modal__toggle-button").click(function() {
     $(".modal__toggle-container").toggle();
   });
+
+  function photoPreview(input) {
+    if(input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $('.img-preview').attr({src: e.target.result, class: "show"});
+        $('.modal__picture__span').html("");
+        $('<span class="modal__picture__remove">Remove photo</span>').insertAfter(".modal__picture__label");
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  };
+
+  function removePhoto() {
+    $('.img-preview').attr({src: "#"});
+    $('.modal__field__photo').val(function() {
+      return "";
+    });
+  };
+
+  $('.modal__picture').on('click', ".modal__picture__remove", removePhoto());
+
+  $('.modal__field__photo').change(function(){
+    photoPreview(this);
+  });
+
+
 });
