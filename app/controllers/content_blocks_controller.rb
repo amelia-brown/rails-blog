e8af5549ce7f42c315192400bc36e9f19cb90bcc
Amelia Brown <amelia.brown389@gmail.com>
2016-05-08 20:36:22 -0400

    (feature): content_blocks update action

--- app/controllers/content_blocks_controller.rb
+++ app/controllers/content_blocks_controller.rb
@@ -1,14 +1,6 @@
 class ContentBlocksController < ApplicationController
   def update
-    @page = request.env['PATH_INFO']
-    if @page === "/about"
-      @page = Page.find(1)
-    elsif @page === '/work'
-      @page = Page.find(2)
-    end
-
-    @content_block = @page.content_blocks.find(1)
-    @post.update_attributes(content_block_params)
-    redirect_to @page
+    @page = Page.find(params[:page])
+    @content_block = @page.content_blocks.find(params[:id])
   end
 end
