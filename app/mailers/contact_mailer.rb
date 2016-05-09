98a6082bfabcceb3b02a68fb75f3b6526c5b4e18
Amelia Brown <amelia.brown389@gmail.com>
2016-05-09 14:46:53 -0400

    (feature): mailer method/previews

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,7 @@
 class ContactMailer < ApplicationMailer
+  def contact_email(sender)
+    @sender = sender
+    @user = User.first
+    mail to: @user.email, subject: "New message from #{sender[:name]}"
+  end
 end
--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
-
+  def message_email
+    joe = {:name => "Joe", :email => "joe@joe.ca", :message => "hi"}
+    ContactMailer.contact_email(joe)
+  end
 end
