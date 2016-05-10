12fcdb52a47ccd16742161ecf72b355034eeb978
Amelia Brown <amelia.brown389@gmail.com>
2016-05-10 16:19:01 -0400

    (styles): work and about

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ end
 User.create!(name: "Example User", email: "amelia.brown389@gmail.com", password: "example")
 
 about.content_blocks.create!(content: paragraphs)
-work.content_blocks.create!(content: paragraphs)
-work.content_blocks.create!(content: paragraphs)
-work.content_blocks.create!(content: paragraphs)
+work.content_blocks.create!(content: "<p>Are you looking to have compelling content that aligns with your brand’s values? I can create content that fits your vision.</p>")
+work.content_blocks.create!(content: "<p>Are you looking for expert advice and recommendations on the planning, development, and management of your content? I can help you navigate some important decisions and develop the most suitable content strategy.</p>")
+work.content_blocks.create!(content: "<p>Do you have content that you feel needs to be improved and polished? I can edit your content and get it ready for publication.</p>")
 
 
 12.times do
